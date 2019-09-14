4ca793a96c9933eed6dffd76e1f44f31c383d0c7
wangyanqi <553534417@qq.com>
2019-09-14 23:33:12 +0800

    9.14

--- resources/views/admin/layout.blade.php
+++ resources/views/admin/layout.blade.php
@@ -4,7 +4,7 @@
 <html>
 <head>
     <meta charset="utf-8">
-    <title>王延琦技术博客后台管理系统</title>
+    <title>王延琦技术博客后台</title>
     <meta name="renderer" content="webkit">
     <meta http-equiv="X-UA-Compatible" content="IE=edge,chrome=1">
     <meta name="viewport" content="width=device-width, initial-scale=1.0, minimum-scale=1.0, maximum-scale=1.0, user-scalable=0">
